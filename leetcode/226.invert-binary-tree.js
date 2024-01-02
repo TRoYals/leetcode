834bc82e8ea808a4909c1e71155e9b813816ff37
TRoYals <aozora00321@gmail.com>
2024-01-02 23:33:11 +0800

    binary tree reverse

---
+++ leetcode/226.invert-binary-tree.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=226 lang=javascript
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (76.21%)
+ * Likes:    13488
+ * Dislikes: 197
+ * Total Accepted:    1.9M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  let stack = [root];
+  while (stack.length > 0) {
+    let curr = stack.pop();
+    stack.push(curr);
+
+    let temp = curr.left;
+    curr.left = curr.right;
+    curr.left = temp;
+
+    if (curr.left) {
+      stack.push(curr.left);
+    }
+    if (curr.right) {
+      stack.push(curr.right);
+    }
+  }
+  return root;
+};
+// @lc code=end
